582b5ff6ae2e9364725b622c110d94fbd0d4017e
Bien <bien2004official@gmail.com>
2020-08-25 22:24:13 +0700

    script: default using all cpu threads available

--- yumi.sh
+++ yumi.sh
@@ -27,9 +27,9 @@ for device in $*; do
   esac
   echo Building kernel for $device...
   make clean && make mrproper ;
-  rm -rf ./Yumi/out ;
+  rm -rf ./Yumi/Out ;
   make O=./Yumi/Out $DVNAME ;
-  make O=./Yumi/Out -j2 ;
+  make O=./Yumi/Out -j$(nproc --all) ;
   sudo cp -r ./Yumi/Q/ramdisk ./Yumi/AIK/ramdisk ;
   cp -r ./Yumi/Q/split_img ./Yumi/AIK/split_img ;
   mv ./Yumi/Out/arch/arm64/boot/Image ./Yumi/AIK/split_img/boot.img-zImage ;
@@ -38,4 +38,5 @@ for device in $*; do
   mkdir ./Yumi/Product
   mv ./Yumi/AIK/image-new.img ./Yumi/Product/YumiKernel-V1.0-$device-$(date +'%Y%m%d').img ;
   ./Yumi/AIK/cleanup.sh ;
+  echo Kernel build complete for $device
 done
