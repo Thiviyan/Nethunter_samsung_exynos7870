19bb1689fdd14cb9db1110ec68f0bf3a4a603930
Bien <bien@bienserver.bien2004official>
2020-08-25 22:19:17 +0700

    script: some edits

--- yumi.sh
+++ yumi.sh
@@ -1,21 +1,41 @@
 #!/bin/bash
 
 export ARCH=arm64
-export CROSS_COMPILE=~/Android/Toolchains/gcc-linaro-7.5.0/bin/aarch64-linux-gnu-
+export CROSS_COMPILE=/target/gcc-linaro-7.5.0/bin/aarch64-linux-gnu-
 export ANDROID_MAJOR_VERSION=q
 export ANDROID_PLATFORM_VERSION=10
+export CCACHE_DIR=/target/.ccache
 
 for device in $*; do
+  case $device in
+    A320X) DVNAME=exynos7870-a3y17lte_defconfig
+	   ;;
+    A600X) DVNAME=exynos7870-a6lte_defconfig
+  	   ;;
+    J530X) DVNAME=exynos7870-j5y17lte_defconfig
+	   ;;
+    J600X) DVNAME=exynos7870-j6lte_defconfig
+	   ;;
+    J701X) DVNAME=exynos7870-j7velte_defconfig
+	   ;;
+    J710X) DVNAME=exynos7870-j7xelte_defconfig
+	   ;;
+    J730X) DVNAME=exynos7870-j7y17lte_defconfig
+	   ;;
+    G610X) DVNAME=exynos7870-on7xelte_defconfig
+	   ;;
+  esac
+  echo Building kernel for $device...
   make clean && make mrproper ;
-  rm -rf out ;
-  make O=./Yumi/Out $device ;
-  make O=./Yumi/Out -j64 ;
+  rm -rf ./Yumi/out ;
+  make O=./Yumi/Out $DVNAME ;
+  make O=./Yumi/Out -j2 ;
   sudo cp -r ./Yumi/Q/ramdisk ./Yumi/AIK/ramdisk ;
   cp -r ./Yumi/Q/split_img ./Yumi/AIK/split_img ;
   mv ./Yumi/Out/arch/arm64/boot/Image ./Yumi/AIK/split_img/boot.img-zImage ;
   mv ./Yumi/Out/arch/arm64/boot/dtb.img ./Yumi/AIK/split_img/boot.img-dt ;
-  sudo -S ./Yumi/AIK/repackimg.sh ;
+  sudo ./Yumi/AIK/repackimg.sh ;
   mkdir ./Yumi/Product
-  mv ./Yumi/AIK/image-new.img ./Yumi/Product/YumiKernel_V1.0-"$*".img ;
+  mv ./Yumi/AIK/image-new.img ./Yumi/Product/YumiKernel-V1.0-$device-$(date +'%Y%m%d').img ;
   ./Yumi/AIK/cleanup.sh ;
 done
