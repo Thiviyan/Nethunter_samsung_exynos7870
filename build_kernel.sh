77a422db2d802abebe5411303746f2568d96719e
S133PY <thiviyan@gmail.com>
2020-09-07 22:56:31 +0530

    add toolchain path to build script

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 export ARCH=arm64
-export CROSS_COMPILE=/media/astrako/DATOS/toolchain/gcc-linaro-7.5.0/bin/aarch64-linux-gnu-
+export CROSS_COMPILE=~/Android/Toolchains/linaro-7.5.0-aarch64-linux/bin/aarch64-linux-gnu-
 export ANDROID_MAJOR_VERSION=q
 export ANDROID_PLATFORM_VERSION=10
 
-make O=./out $1
+make O=./out exynos7870-on7xelte_defconfig
 make O=./out -j64
