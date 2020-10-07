eeaa40cd57bd9635250d5211815db82963c1025d
S133PY <Thiviyan@gmail.com>
2020-10-07 18:48:21 +0530

    air crack drivers

--- build_kernel.sh
+++ build_kernel.sh
@@ -5,5 +5,6 @@ export CROSS_COMPILE=~/Android/Toolchains/linaro-7.5.0-aarch64-linux/bin/aarch64
 export ANDROID_MAJOR_VERSION=q
 export ANDROID_PLATFORM_VERSION=10
 
-make O=./out exynos7870-on7xelte_defconfig
+make O=./out exynos7870-j7y17lte_defconfig
 make O=./out -j64
+make O=./out -j64 INSTALL_MOD_PATH=MODULES_OUT modules_install
